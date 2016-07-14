0daefef76ee72a1c975e097d8543c25027ef3afe
khaueviana <khaueviana@multconnect.com.br>
2016-07-13 23:44:06 -0300

    melhoria no exemplo do async / await

--- KV.AsyncMethods/Program.cs
+++ KV.AsyncMethods/Program.cs
@@ -2,47 +2,29 @@
 
 namespace KV.AsyncMethods
 {
-    using System.Threading;
     using System.Threading.Tasks;
 
     class Program
     {
         static void Main(string[] args)
         {
-            MyClass myclass = new MyClass();
+            new SampleAsyncClass().Foo1();
 
-            myclass.Foo1();
-
-            while (MyClass.Count != 3) Thread.Sleep(1);
-            foreach (string msg in MyClass.Strings)
-            {
-                Console.WriteLine(msg);
-            }
+            Console.WriteLine("After Foo1 " + DateTime.Now.Millisecond);
 
             Console.ReadKey();
         }
     }
 
-    class MyClass
+    public class SampleAsyncClass
     {
-        public static string[] Strings = new string[3];
-        public static int Count = 0;
-
         public async void Foo1()
-        {
-            Strings[Count++] = "Foo1 at " + DateTime.Now.Millisecond;
-
-            // Call Foo2 asynchronously AND after this function returns.
-            await Foo2();
-
-            Strings[Count++] = "End of Foo1 at " + DateTime.Now.Millisecond;
-        }
-
-        private async Task Foo2()
         {
             await Task.Yield();
 
-            Strings[Count++] = "Foo2 called at " + DateTime.Now.Millisecond;
+            Console.WriteLine("Foo1 at " + DateTime.Now.Millisecond);
+                       
+            Console.WriteLine("End of Foo1 at " + DateTime.Now.Millisecond);
         }
     }
 }
