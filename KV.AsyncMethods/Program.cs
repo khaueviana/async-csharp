328453cc3a7d77d00615fbdaf595fec1415379d7
Khauê Viana <khaueviana@gmail.com>
2017-12-12 08:25:39 -0200

    teste

--- KV.AsyncMethods/Program.cs
+++ KV.AsyncMethods/Program.cs
@@ -2,13 +2,14 @@
 
 namespace KV.AsyncMethods
 {
+    using System.Threading;
     using System.Threading.Tasks;
 
     class Program
     {
         static void Main(string[] args)
         {
-            new SampleAsyncClass().Foo1();
+            new SampleAsyncClass().Foo2Async();
 
             Console.WriteLine("After Foo1 " + DateTime.Now.Millisecond);
 
@@ -18,12 +19,19 @@ namespace KV.AsyncMethods
 
     public class SampleAsyncClass
     {
-        public async void Foo1()
+        public async Task Foo2Async()
         {
-            await Task.Yield();
+            Foo1();
+            
+            Console.WriteLine("Foo3 at " + DateTime.Now.Millisecond);
+        }
+
+        public async Task Foo1()
+        {
+            await Task.Delay(2000);
 
             Console.WriteLine("Foo1 at " + DateTime.Now.Millisecond);
-                       
+
             Console.WriteLine("End of Foo1 at " + DateTime.Now.Millisecond);
         }
     }
